48663698420c13428b5374d59a487f12998c1b5f
505e06b2 <505e06b2@gmail.com>
2016-09-04 06:58:49 +0100

    Inital Commit

---
+++ commands.js
@@ -0,0 +1,39 @@
+var commands = [ //Can't be "required" or window.open works funny.. Might sort it later
+				{
+					"name": "help",
+					"func": function() {
+						print("For all commands, there is no need for (\") as whitespace is ignored")
+						for (var i = 0; i < commands.length; i++) {
+							print(commands[i].name + ": " + commands[i].help);
+						}
+					},
+					"help": "Displays all commands and how to use"
+				},
+				{
+					"name": "echo",
+					"func": function(out) {
+						print(out.join(" "));
+					},
+					"help": "Print something"
+				},
+				{
+					"name": "clear",
+					"func": function() {
+						maintable.innerHTML = "";
+					},
+					"help": "Clear the screen"
+				},
+				{
+					"name": "js",
+					"func": function(s) {
+						s = s.join(" ").trim()
+						if (s.length == 0) {print("No text specified as parameter");return}
+						try {
+							eval(s);
+						} catch(e) {
+							print(e);
+						}
+					},
+					"help": "Run a JS string"
+				}
+			];
---
+++ frontend.js
@@ -0,0 +1,69 @@
+function generateLine() {
+	maintable.innerHTML += "<tr><td><div id='currentline' class='textin'>user@desktop><span id='edit' contenteditable='true'></span></div></td></tr>";
+	currentline = document.getElementById("currentline");
+	currenttext = document.getElementById("edit");
+	currenttext.onkeydown = function(e) {
+		if (ctrlDown && e.keyCode == 67) {
+			currenttext.outerHTML = currenttext.innerHTML;
+			generateLine();
+		}
+	}
+	currenttext.focus();
+}
+
+function print(text) {
+	maintable.innerHTML += "<tr><td>" + text + "</td></tr>";
+}
+
+function processCmds(cmd) {
+	var split = cmd.split(" ");
+	split[0] = split[0].toLowerCase();
+	for (var i = 0; i < commands.length; i++) {
+		if (commands[i].name == split[0]) {
+			commands[i].func(split.splice(1));
+			return
+		}
+	}
+	maintable.innerHTML += "<tr><td>" + split[0] + " is not a command</td></tr>";
+}
+
+//CTRL EVENTS
+var ctrlDown = false;
+window.onkeydown = function(e) {
+	if (e.keyCode == 17 || e.keyCode == 91) { //CTRL or CMD
+		ctrlDown = true;
+	} else if (e.keyCode == 38) { //Up Arrow
+		currenttext.innerHTML = lastcommand;
+	}
+}
+window.onkeyup = function(e) {
+	if (e.keyCode == 17 || e.keyCode == 91) ctrlDown = false;
+}
+
+//PRESS ENTER
+window.onkeypress = function(e) {
+	if ((e.keyCode == 13 || e.key == "Enter" ) && e.target.id == "edit") {
+		e.target.outerHTML = e.target.innerHTML;
+		lastcommand = e.target.textContent;
+		if (e.target.innerHTML != "") processCmds(e.target.textContent);
+		generateLine();
+		e.preventDefault(); //No newline at the end
+	}
+}
+
+window.onclick = function(e) {
+	currenttext.focus();
+}
+
+var maintable;
+var currentline;
+var currenttext;
+var lastcommand = "";
+window.onload = function() {
+	maintable = document.createElement("table");
+	document.body.appendChild(maintable);
+	print("505e06b2 Firmware [Version 1.01]");
+	print("Copyright (c) 2016 505e06b2.  All rights reserved.");
+	print("&nbsp;");
+	generateLine();
+}
